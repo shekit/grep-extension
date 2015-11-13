98a880b614679cf758294835ee5e20e61ffa1715
shekit <as7790@nyu.edu>
2015-11-13 17:38:22 -0500

    gettin active tab url

--- popup.js
+++ popup.js
@@ -4,6 +4,14 @@ console.log("Hello popup");
 // 	$("#weather p").html("bye");
 // },1000);
 
+chrome.tabs.query({
+	active:true,
+	lastFocusedWindow:true
+}, function(tabs){
+	var tab = tabs[0]
+	console.log(tab.url)
+})
+
 $("#searchForm").on('submit', function(event){
 	event.preventDefault();
 	var query = $("#search").val();
