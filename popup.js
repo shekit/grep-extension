ffa78a45505b4b18ee796e703caed8f325d815c0
shekit <as7790@nyu.edu>
2015-11-13 10:09:33 -0500

    chrome extension talking to elastic search

--- popup.js
+++ popup.js
@@ -1,5 +1,23 @@
 console.log("Hello popup");
 
-setTimeout(function(){
-	$("#weather p").html("bye");
-},1000);
+// setTimeout(function(){
+// 	$("#weather p").html("bye");
+// },1000);
+
+$("#searchForm").on('submit', function(event){
+	event.preventDefault();
+	var query = $("#search").val();
+	console.log(query)
+
+	$.ajax({
+		url: 'http://grep.us/search',
+		method: "POST",
+		data: {"type":"p5", "query":query, "from":0}
+	})
+	.done(function(response){
+		console.log(response)
+	})
+	.fail(function(response){
+		console.log("FAILED")
+	})
+})
